b9503835eec2e4acb06fc9fbe7956a9913ecaf65
John Kuras <w7og@yahoo.com>
2019-08-06 06:50:31 -0700

    corrected typo in comment

--- userlist.c
+++ userlist.c
@@ -4,7 +4,7 @@
  *  Created on: Mar 13, 2018
  *      Author: dad
  *
- *	This program reads a set of passwd, shadow, and goup files plus a special input
+ *	This program reads a set of passwd, shadow, and group files plus a special input
  *	file that contains a mapping of the user's passwords and samba share names. The
  *	the files are parsed and a set of scripts are produced that can be used to duplicate
  *	the usernames, passwords, and share locations on a second computer (handy when
